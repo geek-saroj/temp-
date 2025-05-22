97ec6dbe77e01a686be6777c114782729bd2d9be
Saroj <geeksaroj@gmail.com>
2025-05-22 10:54:35 +0545

    commit - 5

--- index.js
+++ index.js
@@ -10,6 +10,10 @@ app.get("/second-route", (req, res) => {
   res.send("Response from second route!");
 });
 
+app.get("/sixth-route", (req, res) => {
+  res.send("Response from sixth route!");
+});
+
 app.listen(3000);
 
 console.log("Server running on port 3000");
